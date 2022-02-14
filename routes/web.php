0a9b5dfa7f6ec78b8ac8d5b3bfb62fe255f68fcf
TECNOLOGIA ALGUARISA <tecnologia@alguarisa.local>
2022-02-14 17:23:37 -0400

    login

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return redirect()->route('login');
+    return view('auth.login2');
+    //return redirect()->route('login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
