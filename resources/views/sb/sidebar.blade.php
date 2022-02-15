f3275322b75b7c2689a610a95ee3a4d16454816c
TECNOLOGIA ALGUARISA <tecnologia@alguarisa.local>
2022-02-15 12:50:43 -0400

    sidebar 2

--- resources/views/sb/sidebar.blade.php
+++ resources/views/sb/sidebar.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="/dashboard">
+        <a class="nav-link" href="{{ route('dashboard') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Inicio</span></a>
     </li>
@@ -36,8 +36,8 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="/prueba">Casos Sociales</a>
-                <a class="collapse-item" href="cards.html">Oficios Recibidos</a>
+                <a class="collapse-item" href="#">Casos Sociales</a>
+                <a class="collapse-item" href="#">Oficios Recibidos</a>
             </div>
         </div>
     </li>
