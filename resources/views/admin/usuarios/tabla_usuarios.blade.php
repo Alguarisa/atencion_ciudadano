f6d32b923b02688c51e9647d592c9d504f0830be
TECNOLOGIA ALGUARISA <tecnologia@alguarisa.local>
2022-02-15 13:02:44 -0400

    lista negrita

--- resources/views/admin/usuarios/tabla_usuarios.blade.php
+++ resources/views/admin/usuarios/tabla_usuarios.blade.php
@@ -1,6 +1,6 @@
 <div class="table-responsive">
     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
+        <thead class="thead-dark">
             <tr>
                 <th>N°</th>
                 <th>Nombre y Apellido</th>
